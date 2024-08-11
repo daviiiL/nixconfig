863490d274dd41e2545c71438fc7303da626b434
daviiiL <davidas99017@gmail.com>
2024-08-10 21:37:23 -0400

    bumping nixpkgs version to 24.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
