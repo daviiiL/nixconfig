be60f052f4bdc6a271bcdca4e8c274de2affa865
daviiiL <davidas99017@gmail.com>
2024-08-12 19:36:50 -0400

    updated wndrportal home.nix

--- home-manager/hosts/wndr/home.nix
+++ home-manager/hosts/wndr/home.nix
@@ -114,6 +114,7 @@
     zsh.shellAliases = {
       la = "eza -la";
       ls = "eza";
+      laa = "eza -l --git -T --hyperlink --header --git-ignore .";
       code = "code --enable-features=UseOzonePlatform --ozone-platform=wayland";
       nixrebuild = "cd ~/nixconfig && sudo nixos-rebuild switch --flake .#wndr";
       switchhome = "cd ~/nixconfig && home-manager switch --flake .#rudeus@wndr";
