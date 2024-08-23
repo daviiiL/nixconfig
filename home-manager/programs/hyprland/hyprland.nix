c28b9dc1e47f6328b79091db7537baace8a07a10
daviiiL <davidas99017@gmail.com>
2024-08-23 17:12:12 -0400

    adding more hyprland plugins

--- home-manager/programs/hyprland/hyprland.nix
+++ home-manager/programs/hyprland/hyprland.nix
@@ -2,6 +2,9 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    plugins = [ inputs.hyprland-plugins.packages."${pkgs.system}".hyprbars ];
+    plugins = with inputs.hyprland-plugins.packages.${pkgs.system}; [
+      hyprbars
+      hyprexpo
+    ];
   };
 }
