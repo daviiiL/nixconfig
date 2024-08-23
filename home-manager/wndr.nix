68dc0936e641ef0f3b552b300aac0ecf37932552
daviiiL <davidas99017@gmail.com>
2024-08-23 17:05:22 -0400

    monitor settings

--- home-manager/wndr.nix
+++ home-manager/wndr.nix
@@ -22,4 +22,8 @@
     nixrebuild = "cd ~/nixconfig && sudo nixos-rebuild switch --flake .#wndr";
     switchhome = "cd ~/nixconfig && home-manager switch --flake .#rudeus@wndr";
   };
+  wayland.windowManager.hyprland.extraConfig = "
+      monitor=HDMI-A-1,3840x2160@120,0x0,1.5,bitdepth,12
+      monitor=DP-2,3840x2160@144,2560x297,2,bitdepth,10
+    ";
 }
