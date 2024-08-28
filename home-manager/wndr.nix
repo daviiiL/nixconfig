3ebcf2730e58dd0b6af1f48d5032346a95748f06
David Liu <davidas99017@gmail.com>
2024-08-28 04:05:10 -0400

    changes

--- home-manager/wndr.nix
+++ home-manager/wndr.nix
@@ -23,13 +23,13 @@
     switchhome = "cd ~/nixconfig && home-manager switch --flake .#rudeus@wndr";
   };
   wayland.windowManager.hyprland.extraConfig = "
-      monitor=HDMI-A-2,3840x2160@120,0x0,1.5,bitdepth,12
-      # monitor=DP-2,3840x2160@144,2560x297,2,bitdepth,10
+      monitor=HDMI-A-1,3840x2160@120,0x0,1.5,bitdepth,10
+      monitor=DP-1,3840x2160@144,2560x297,2,bitdepth,10
     
-      # workspace = 1, monitor:HDMI-A-1, default:true, rounding:true
-      # workspace = 2, monitor:HDMI-A-1, rounding: true 
-      # workspace = 3, monitor:HDMI-A-1, rounding: true 
-      # workspace = 4, monitor:HDMI-A-1, rounding: true 
-      # workspace = 5, monitor:HDMI-A-1, rounding: true
+      workspace = 1, monitor:HDMI-A-1, default:true, rounding:true
+      workspace = 2, monitor:HDMI-A-1, rounding: true 
+      workspace = 3, monitor:HDMI-A-1, rounding: true 
+      workspace = 4, monitor:HDMI-A-1, rounding: true 
+      workspace = 5, monitor:HDMI-A-1, rounding: true
     ";
 }
