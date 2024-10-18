51a331f23e1eae6677150053d52ed3fde82514ab
David Liu <davidas99017@gmail.com>
2024-10-17 23:38:55 -0400

    kmscon config

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland";
     hyprlock.url = "github:hyprwm/hyprlock";
     nixvim.url = "github:daviiiL/nixvim";
 
--- home-manager/hosts/wndr.nix
+++ home-manager/hosts/wndr.nix
@@ -40,8 +40,8 @@
     switchhome = "cd ~/nixconfig && home-manager switch --flake .#rudeus@wndr";
   };
   wayland.windowManager.hyprland.extraConfig = "
-      monitor=HDMI-A-1,3840x2160@120,0x0,1.5
-      monitor=DP-1, 3840x2160@144, 2560x297,2
+      monitor=HDMI-A-1,3840x2160@120,0x0,1.5,bitdepth,10
+      monitor=DP-1, 3840x2160@144, 2560x297,2,bitdepth,10
     
       workspace = 1, monitor:HDMI-A-1, default:true, rounding:true
       workspace = 2, monitor:HDMI-A-1, rounding: true 
