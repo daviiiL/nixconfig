bb578069f18e4b1e73a76863d40fad457798e48b
David Liu <davidas99017@gmail.com>
2024-10-20 18:08:51 -0400

    Added devenv cachix

--- nixos/components/dev.nix
+++ nixos/components/dev.nix
@@ -11,4 +11,9 @@ in {
   environment.systemPackages = [
     unstable-pkgs.devenv
   ];
+
+  nix.extraOptions = "
+      extra-substituters = https://devenv.cachix.org
+      extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+    ";
 }
