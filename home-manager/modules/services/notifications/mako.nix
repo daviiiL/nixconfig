af759465439732392f8397a1523884a997b6f75a
David Liu <davidas99017@gmail.com>
2024-10-19 20:11:10 -0400

    mako restyle

--- home-manager/modules/services/notifications/mako.nix
+++ home-manager/modules/services/notifications/mako.nix
@@ -7,7 +7,7 @@
     backgroundColor = lib.mkForce "#11111bee";
     borderColor = lib.mkForce "#89b4fa";
     borderRadius = lib.mkForce 10;
-    font = "Inter 28pt,Inter 28pt 13";
+    font = "Inter 28pt,Inter 28pt 12";
     padding = "10, 20";
   };
 }
