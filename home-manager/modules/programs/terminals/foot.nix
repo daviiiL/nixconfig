79b3938773ce232291f417e0e23d2437f46cbccf
David Liu <davidas99017@gmail.com>
2024-10-28 14:52:06 -0400

    Updated sway

--- home-manager/modules/programs/terminals/foot.nix
+++ home-manager/modules/programs/terminals/foot.nix
@@ -1,11 +1,16 @@
 {
   programs.foot = {
     enable = true;
+    catppuccin.enable = false;
     settings = {
       main = {
         term = "xterm-256color";
         font = "FiraCode Nerd Font:size=11";
         pad = "10x10 center";
+      };
+      colors = {
+        alpha = 0.95;
+        background = 141424;
       };
     };
   };
