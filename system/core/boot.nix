1f7dd424dd6ec55ff2c35df1c28b21570b15c145
chronos <davidas99017@gmail.com>
2025-02-04 20:52:46 -0500

    feat(boot): plymouth

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -1,6 +1,18 @@
 {pkgs, ...}: {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    loader = {
+      #systemd-boot
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    #using the latest kernel instead of 6.6
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    #plymouth
+    plymouth = {
+      enable = true;
+    };
+  };
 }
