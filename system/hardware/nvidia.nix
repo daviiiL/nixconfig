9c36f8f1b9023e928bdc93a211c9d07d4db98d4c
David Liu <davidas99017@gmail.com>
2025-02-09 23:52:41 -0500

    feat(theme): update

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -28,7 +28,7 @@ in {
       prime = {
         offload.enable = true;
         intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:01:00:0";
+        nvidiaBusId = "PCI:1:0:0";
       };
     };
   };
