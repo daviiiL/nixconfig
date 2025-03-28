9e1bbbfff92425cdf79d672560da036153e30f29
David Liu <davidas99017@gmail.com>
2025-03-28 19:32:38 -0400

    update

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -9,7 +9,18 @@
     config.allowUnfree = true;
   };
 in {
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
+  boot.kernelPackages =  pkgs.linuxPackagesFor (
+    pkgs.linuxKernel.kernels.linux_6_12.override {
+      argsOverride = rec {
+        src = pkgs.fetchurl {
+          url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+	  hash = "sha256-6Y1C0X73Bjs/LW12kr8kiZ4uAhz4MtGbVTCOyOjgjv8=";
+        };
+        version = "6.12.12";
+        modDirVersion = "6.12.12";
+      };
+    }
+  );
   hardware = {
     graphics = {
       enable = true;
