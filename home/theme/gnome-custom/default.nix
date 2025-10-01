d08b461d4b2c7e03858dde59355b2d35c6077024
David Liu <davidas99017@gmail.com>
2025-09-30 17:34:00 -0700

    Updated hm settings

--- home/theme/gnome-custom/default.nix
+++ home/theme/gnome-custom/default.nix
@@ -4,23 +4,23 @@
     ./program-themes
   ];
 
-  gtk = {
-    enable = true;
-    iconTheme = {
-      name = "WhiteSur-dark";
-      package = pkgs.whitesur-icon-theme;
-    };
-    theme = {
-      name = "WhiteSur-Dark-solid";
-      package = pkgs.whitesur-gtk-theme;
-    };
-  };
-
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-    package = pkgs.whitesur-cursors;
-    name = "WhiteSur-cursors";
-    size = 24;
-  };
+  # gtk = {
+  #   enable = true;
+  #   iconTheme = {
+  #     name = "WhiteSur-dark";
+  #     package = pkgs.whitesur-icon-theme;
+  #   };
+  #   theme = {
+  #     name = "WhiteSur-Dark-solid";
+  #     package = pkgs.whitesur-gtk-theme;
+  #   };
+  # };
+  #
+  # home.pointerCursor = {
+  #   gtk.enable = true;
+  #   x11.enable = true;
+  #   package = pkgs.whitesur-cursors;
+  #   name = "WhiteSur-cursors";
+  #   size = 24;
+  # };
 }
